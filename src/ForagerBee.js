5ac5f15015ae7cb9f782e94a1867c86a56f40e56
Poisk <vpisotski@gmail.com>
2018-05-09 11:24:41 -0700

    zz

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,10 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee{
+  constructor(food, age, color, job) {
+  super(food)
+  this.age = 10
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
+  }
+  forage(treasure) {this.treasureChest.push(treasure)};
 };
